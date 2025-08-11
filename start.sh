99ff34104bee151155b8fcbd96d515da9ff69ead
MoonlightPanel <haminhchiennoi@gmail.com>
2025-08-11 09:56:38 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,10 +8,10 @@ SEED="/opt/qemu/seed.iso"
  
 echo "Creating VM disk..."
 qemu-img convert -f qcow2 -O raw "$IMG" "$DISK"
-qemu-img resize "$DISK" 30G
+qemu-img resize 128 30G
 # Start VM
 qemu-system-x86_64 \
-    -m "$RAM"G \
+    -m 12G \
     -drive file="$DISK",format=raw,if=virtio \
     -drive file="$SEED",format=raw,if=virtio \
     -netdev user,id=net0,hostfwd=tcp::2222-:22 \
@@ -30,12 +30,5 @@ echo " 🧾 Login: root / root"
 echo " Supported Code Sandbox (use ngrok or cloudflare)"
 echo " Code By Snipavn/Snhvn (Github) Youtube: https://youtube.com/@snipavn205 & Youtube: HopingBoyz" 
 echo "================================================"
-
-# Wait for SSH port to be ready
-for i in {1..30}; do
-  nc -z localhost 2222 && echo "✅ VM is ready!" && break
-  echo "⏳ Waiting for SSH..."
-  sleep 2
-done
 
 wait
